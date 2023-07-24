b31c9800895948b46db65658e2cd0a4ccec269f4
Rima Skrabiene <r.skrabiene@gmai.com>
2023-07-24 21:25:07 +0300

    1 paskaita

---
+++ paskaita 1/index.js
@@ -0,0 +1,23 @@
+console.log(123)
+
+const name = "Rima";
+
+let age = 26;
+console.log(age);
+age = 30
+console.log(age);
+console.log(name);
+
+const sum = 10+15;
+console.log(sum);
+
+const sub = 10 - 3;
+console.log(sub);
+
+const mult = 3 *5 ;
+console.log(mult);
+
+const sub3 = age + 7;
+console.log(sub3);
+console.log(age);
+
---
+++ paskaita 1/uzduotis1/index.js
@@ -0,0 +1,24 @@
+// // -------Uzduotis 1
+
+// console.log("Hello world!");
+// alert("I love JavaScript");
+// alert ("Redy to learn more?");
+// const Number = prompt("Įversk skaičius nuo 1 iki 10");
+// console.log(Number);
+
+// --------uzduotis 2
+
+// const name = prompt("iveskite savo varda")
+// const birthDay = "Geguzes 12"
+
+// const fullText =name + " yra gimes " + birthDay
+// console.log(fullText)
+
+// ----------Uzduotis 3
+
+const bazinisUzmokestis = prompt(" iveskite bazini uzmokesti ");
+const workingPeople = 1761463;
+const budget = 8487300000;
+const precent = (workingPeople * bazinisUzmokestis * 100) / budget;
+console.log(precent);
+document.write(precent);
