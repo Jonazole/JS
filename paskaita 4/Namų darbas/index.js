a87cfc31aea10526cb0fbe407439ce80b43a12de
Rima Skrabiene <r.skrabiene@gmai.com>
2023-08-01 20:00:08 +0300

    6 pamoka

--- paskaita 4/Namų darbas/index.js
+++ paskaita 4/Namų darbas/index.js
@@ -1,19 +1,19 @@
 // Pirma uzduotis.
 
-const arr = [5,8,1, 6, -10, 5, 100,13,55,56, -6];
+const arr1 = [5,8,1, 6, -10, 5, 100,13,55,56, -6];
 let even = [];
-for (let index = 0; index < arr.length; index++) {
-    if(arr[index] % 2 === 0) {
-    even.push(arr[index])
+for (let index = 0; index < arr1.length; index++) {
+    if(arr1[index] % 2 === 0) {
+    even.push(arr1[index])
     }
    }
 console.log(even);
 // Antra uzduotis.
 
-const array = [5,8,1, 6, -10, 5, 100,13,55,56, -6];
-let suma = 0
-for (let index = 0; index < array.length; index++) 
-if (suma + array[index] = suma)
-
+const array1 = [5, 8, 1, 6, -10, 5, -100, 55, 56, -6];
+let suma = 0;
+for (let index = 0; index < array1.length-1; index++) {
+suma += array1[index] }
 
 console.log(suma);
+
